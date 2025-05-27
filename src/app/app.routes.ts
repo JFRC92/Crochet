914ff3806de8e1482792a9f3a72d76c801a3665e
JFRC92 <jfranciscoruizcabello@gmail.com>
2025-05-27 14:31:08 +0200

    apartadodiseños

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { PatronesComponent } from './components/patrones/patrones.component';
+import { DisenoComponent } from './components/diseno/diseno.component';
 
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'patrones', component: PatronesComponent },
+  { path: 'diseno', component: DisenoComponent},
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/diseno/diseno.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { LogoComponent } from '../logo/logo.component';
+
+
+@Component({
+  selector: 'app-diseno',
+  
+  imports: [
+    RouterModule,
+    LogoComponent,
+    NavigationBarComponent,
+    
+  ],
+
+  standalone: true,
+  templateUrl: './diseno.component.html',
+  styleUrls: ['./diseno.component.css']
+})
+export class DisenoComponent {
+  Disenos = [
+
+  ]
+
+}
