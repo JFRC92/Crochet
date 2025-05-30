305dd5dca1b5fa1384717fbddc3b1b1b3315ee1e
JFRC92 <jfranciscoruizcabello@gmail.com>
2025-05-30 12:40:44 +0200

    nuevos cambios

--- src/app/components/scroll-img/scroll-img.component.ts
+++ src/app/components/scroll-img/scroll-img.component.ts
@@ -6,11 +6,11 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './scroll-img.component.html',
-  styleUrl: './scroll-img.component.css'
+  styleUrls: ['./scroll-img.component.css']
 })
 export class ScrollImgComponent implements OnInit {
   images = [
-    { src: '/images/madrid/equipo.png', alt: 'Equipo crochet at trois' },
+    { src: '/images/madrid/equipo.png', alt: 'Equipo crochet at trois'},
     { src: '/images/madrid/equipo1.png', alt: 'Equipo de crochet luciendo chaleco loopy' },
     { src: '/images/madrid/mariapose.png', alt: 'Maria con top de crochet at trois' },
     { src: '/images/madrid/aranza.png', alt: 'Aranza con top de crochet at trois' },
@@ -18,7 +18,10 @@ export class ScrollImgComponent implements OnInit {
     { src: '/images/bebe.png', alt: 'Conjunto zapatos y diadema bebe' },
     { src: '/images/beaferia.png', alt: 'Manto de feria' },
     { src: '/images/equipo.png', alt: 'Zapatillas bebe' },
-    { src: '/images/8m.png', alt: 'Cristina representando el 8m con su top' }
+    { src: '/images/8m.png', alt: 'Cristina representando el 8m con su top' },
+    { src: '/images/Tir1.png', alt:'Aranza con su chaleco loopy'},
+    { src: '/images/mantoncris.png', alt: 'Cris con su mantón falso de Crochet AT Trois'},
+    { src:'/images/Top.png' , alt:'María con su top de Crochet AT Trois'}
   ];
 
   imagesAll: { src: string; alt: string }[] = [];
